508388fd30602cd915bc0b743867be8301affb99
Mia Frank <31712634+miafrank@users.noreply.github.com>
2026-04-30 13:27:42 -0500

    update Vehicle constructor to accept laneType parameter to track which lane each car is in

--- vehicle.ts
+++ vehicle.ts
@@ -1,9 +1,13 @@
+import { LaneType } from "./enums/laneType";
+
 export class Vehicle {
   id: number;
+  laneType: LaneType;
 
-  constructor(id: number) {
+  constructor(id: number, laneType: LaneType) {
     const vehicleID = Math.floor(Math.random() * 1000);
-    
+
     this.id = vehicleID;
+    this.laneType = laneType;
   }
 }
