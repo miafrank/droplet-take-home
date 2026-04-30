27dccbcaf904b7293da6cfb46247865355e382e3
Mia Frank <31712634+miafrank@users.noreply.github.com>
2026-04-30 13:25:41 -0500

    add walkRequested property and requestWalk method

--- crosswalk.ts
+++ crosswalk.ts
@@ -4,12 +4,19 @@ import { PedestrianSignal } from "./enums/pedestrianSignal";
 export class Crosswalk {
   laneDirection: LaneDirection;
   pedestrianSignal: PedestrianSignal;
+  walkRequested: boolean;
 
   constructor(
     laneDirection: LaneDirection,
     pedestrianSignal: PedestrianSignal,
+    walkRequested: boolean,
   ) {
     this.laneDirection = laneDirection;
     this.pedestrianSignal = pedestrianSignal;
+    this.walkRequested = walkRequested;
+  }
+
+  requestWalk() {
+    this.walkRequested = true;
   }
 }
