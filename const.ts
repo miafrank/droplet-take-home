0ea531b33b02bc1478a1d741a9658b464c9152a9
Mia Frank <31712634+miafrank@users.noreply.github.com>
2026-04-30 20:01:52 -0500

    Rename constants file

--- const.ts
+++
@@ -1,3 +0,0 @@
-export const laneLengthSize = 24;
-export const deltaSeconds = 1;
-export const targetSpeedMph = 15;
---
+++ constants.ts
@@ -0,0 +1,7 @@
+export const laneLengthSize = 24;
+export const deltaSeconds = 1;
+export const targetSpeedMph = 15;
+export const minimumVehicleWeightLbs = 2500;
+export const maximumVehicleWeightLbs = 5000;
+export const smartSensorTriggerWeightLbs = 7500;
+export const smartSensorRedWaitMs = 120_000;
